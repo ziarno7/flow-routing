14eedac8b106f0984b7d454ef2b41c5e997ff363
ziarno7 <filip@jarno.pl>
2016-06-03 20:35:00 +0200

    add version the for modules package

--- package.js
+++ package.js
@@ -30,10 +30,11 @@ Package.onUse(function(api) {
   ], ['client', 'server']);
 
   api.use([
-     'react@0.14.1_1',
-     'kadira:blaze-layout@2.3.0',
-     'kadira:react-layout@1.5.2',
-     'gwendall:blaze-to-react@0.1.2'
+    'react@0.14.1_1',
+    'kadira:blaze-layout@2.3.0',
+    'kadira:react-layout@1.5.2',
+    'gwendall:blaze-to-react@0.1.2',
+    'modules@0.6.1'
   ], ['client', 'server'], { weak: true });
 
   api.addFiles([
